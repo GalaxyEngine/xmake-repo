20e598e48663f49131004642ddfefb37ad483a7d
Romain Bourgogne <50773342+Maisquasar@users.noreply.github.com>
2023-06-12 15:32:17 +0200

    Update xmake.lua

--- packages/g/galaxymath/xmake.lua
+++ packages/g/galaxymath/xmake.lua
@@ -4,11 +4,9 @@ package("galaxymath")
     set_description("Static Math Library for Galaxy Engine")
     set_policy("package.strict_compatibility", true)
 
-    add_urls("https://github.com/GalaxyEngine/GalaxyMath.git")
+    add_urls("https://github.com/GalaxyEngine/GalaxyMath.git
 
-    add_includedirs("include")
-
-    add_versions("06.12.2023", "8e6511e3d9198a07a638b74385f2cfc9f5458ff1")
+    add_versions("v2", "8e6511e3d9198a07a638b74385f2cfc9f5458ff1")
     
     on_install(function (package)
         import("package.tools.xmake").install(package)
