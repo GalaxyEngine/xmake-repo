93dbc6aa92297915b39a5c8542f6017bebf6a66f
Romain Bourgogne <r.bourgogne@student.isartdigital.com>
2023-11-28 11:52:03 +0100

    [Edited] xmake.lua

--- packages/g/galaxymath/xmake.lua
+++ packages/g/galaxymath/xmake.lua
@@ -1,15 +1,15 @@
 package("galaxymath")
-    set_kind("static")
+    set_kind("headeronly")
     set_homepage("https://github.com/GalaxyEngine/GalaxyMath")
-    set_description("Static Math Library for Galaxy Engine")
+    set_description("Header only Math Library for Galaxy Engine")
     set_policy("package.strict_compatibility", true)
 
     add_urls("https://github.com/GalaxyEngine/GalaxyMath.git")
 
-    add_versions("v2", "8e6511e3d9198a07a638b74385f2cfc9f5458ff1")
+    add_versions("1.0", "ce572412e5bb7c98b957fe58f2d00ee58a1fa9cf")
     
     on_install(function (package)
-        import("package.tools.xmake").install(package)
+        os.cp("include", package:installdir())
     end)
 
     on_test(function (package)
