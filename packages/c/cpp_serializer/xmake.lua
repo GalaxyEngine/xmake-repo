5c9f708f4108697d33f92090bcfc5e1bff58e8cc
Maisquasar <romainbou@icloud.com>
2024-02-11 07:53:31 +0100

    [Add] v1.1 for cpp_Serializer lib

--- packages/c/cpp_serializer/xmake.lua
+++ packages/c/cpp_serializer/xmake.lua
@@ -6,6 +6,7 @@ package("cpp_serializer")
     add_urls("https://github.com/Maisquasar/CppSerializer.git")
 
     add_versions("1.0", "908038c832aff9927078a57639bf4af91d5a5429")
+    add_versions("1.1", "11951ddd307ed790cbbdb8e723d1241ed15dc4a7")
 
     add_includedirs("include", "include/cpp_serializer")
     
