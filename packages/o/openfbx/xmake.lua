06eb6cfb40d13bde8076f83cc0c2fc4a1d5c5571
Maisquasar <romainbou@icloud.com>
2024-02-28 17:29:40 +0100

    [Fix?] OpenFBX

--- packages/o/openfbx/xmake.lua
+++ packages/o/openfbx/xmake.lua
@@ -2,19 +2,29 @@ package("openfbx")
     set_homepage("https://github.com/nem0/OpenFBX")
     set_description("Lightweight open source FBX importer")
     set_license("MIT")
+    add_includedirs("include", "include/openfbx")
 
     add_urls("git@github.com:nem0/OpenFBX.git")
     add_versions("2024.02.16", "eefc3df905f762226df980ca95a6c4bac1d0c1ae")
 
-    add_deps("cmake")
-
     on_install(function (package)
         local configs = {}
-        table.insert(configs, "-DCMAKE_BUILD_TYPE=" .. (package:is_debug() and "Debug" or "Release"))
-        table.insert(configs, "-DBUILD_SHARED_LIBS=" .. (package:config("shared") and "ON" or "OFF"))
-        
-        -- Add this line to specify the toolset
-        table.insert(configs, "-T host=x64 -A x64")
+        io.writefile("xmake.lua", [[
+            add_rules("mode.release", "mode.debug")
 
-        import("package.tools.cmake").install(package, configs)
+            target("openfbx")
+                set_kind("static")
+
+                add_includedirs("src")
+                add_headerfiles("src/**.h")
+                add_files("src/**.cpp")
+                add_files("src/**.c")
+
+                if (is_plat("linux")) then
+                    add_cxxflags("-fPIC")
+                end
+        ]])
+        import("package.tools.xmake").install(package, configs)
+
+        os.cp("src/*.h", package:installdir("include/openFBX"))
     end)
