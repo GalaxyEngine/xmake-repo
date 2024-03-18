24bbe21a25eb85af111813b9751d32e8787df267
Romain Bourgogne <50773342+Maisquasar@users.noreply.github.com>
2024-03-18 12:07:29 +0100

    [Add] v1.2 of cpp_serializer

--- packages/c/cpp_serializer/xmake.lua
+++ packages/c/cpp_serializer/xmake.lua
@@ -7,6 +7,7 @@ package("cpp_serializer")
 
     add_versions("1.0", "908038c832aff9927078a57639bf4af91d5a5429")
     add_versions("1.1", "e026931007bfddb36bce787fc8d822ad09d2932d")
+    add_versions("1.2", "905028d80bb7374bbff05ca3c99c9cc2e6c0702f")
 
     add_includedirs("include", "include/cpp_serializer")
     
