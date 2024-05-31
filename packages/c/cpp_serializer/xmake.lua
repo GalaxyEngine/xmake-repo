a1283268da50d2725d141bc9bc4dbbeff76ff3c2
Romain Bourgogne <50773342+Maisquasar@users.noreply.github.com>
2024-05-31 13:03:27 +0200

    Update xmake.lua of cpp_serializer to add v1.3

--- packages/c/cpp_serializer/xmake.lua
+++ packages/c/cpp_serializer/xmake.lua
@@ -8,6 +8,7 @@ package("cpp_serializer")
     add_versions("1.0", "908038c832aff9927078a57639bf4af91d5a5429")
     add_versions("1.1", "e026931007bfddb36bce787fc8d822ad09d2932d")
     add_versions("1.2", "905028d80bb7374bbff05ca3c99c9cc2e6c0702f")
+    add_versions("1.3", "c8c154428cd60fe55e368c9c07ee73604ce6cdee")
 
     add_includedirs("include", "include/cpp_serializer")
     
